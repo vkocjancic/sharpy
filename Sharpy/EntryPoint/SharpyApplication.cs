7167624b4ebeb79f81d24a3acecc27a8fadbdf46
vkocjancic <kocjancic@hotmail.com>
2023-12-27 16:57:48 +0100

    Added comments to classes

--- Sharpy/EntryPoint/SharpyApplication.cs
+++ Sharpy/EntryPoint/SharpyApplication.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Sharpy.EntryPoint
 {
+    /// <summary>
+    /// Main Sharpy application class. This is all you need to run sharpy engine app.
+    /// </summary>
     public class SharpyApplication
     {
 
         #region Public methods
 
+        /// <summary>
+        /// Main function with game loop.
+        /// </summary>
         public void Run()
         {
             Console.WriteLine("Hellow from Sharpy app");
