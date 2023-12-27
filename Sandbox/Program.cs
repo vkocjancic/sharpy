faafe97c4e01f5a3eeb03671d51d30c5bebd2aee
vkocjancic <kocjancic@hotmail.com>
2023-12-27 16:54:00 +0100

    Initial commit

---
+++ Sandbox/Program.cs
@@ -0,0 +1,10 @@
+﻿using Sharpy.EntryPoint;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var app = new SharpyApplication();
+        app.Run();
+    }
+}
---
+++ Sharpy/EntryPoint/SharpyApplication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpy.EntryPoint
+{
+    public class SharpyApplication
+    {
+
+        #region Public methods
+
+        public void Run()
+        {
+            Console.WriteLine("Hellow from Sharpy app");
+        }
+
+        #endregion
+
+    }
+}
